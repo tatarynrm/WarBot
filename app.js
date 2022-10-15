4723bacec51bd40cc52b31c115e1b6462f457d16
tatarynrm <tatarynrm@gmail.com>
2022-10-15 23:04:52 +0300

    ds

--- app.js
+++ app.js
@@ -1,84 +1,78 @@
-const { Telegraf } = require('telegraf')
-const fs = require('fs');
-const axios = require('axios');
-const path = require('path');
-require('dotenv').config();
+const { Telegraf } = require("telegraf");
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
+require("dotenv").config();
 
-const User = require('./db/models/userModel');
+const User = require("./db/models/userModel");
 
-const { createIfNotExist } = require('./services/user.service.js');
-const { validateDateFormat } = require('./services/validation.service.js');
-const { createImageWithToday, getDAta } = require('./services/image.service.js');
-const { todayDate } = require('./services/date.service');
-const sequelize = require('./db/index');
+const { createIfNotExist } = require("./services/user.service.js");
+const { validateDateFormat } = require("./services/validation.service.js");
+const {
+  createImageWithToday,
+  getDAta,
+} = require("./services/image.service.js");
+const { todayDate } = require("./services/date.service");
+const sequelize = require("./db/index");
 
-( async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-})()
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(async (ctx) => {
-    const user = ctx.message.from;
+  const user = ctx.message.from;
 
-    createIfNotExist(user);
-    bot.telegram.sendMessage(ctx.chat.id,
-        `Привіт ${ctx.message.from.username} 
-цей телеграм бот показує статистику втрат рашистів`, {
-        reply_markup: {
-            keyboard: [
-                [
-                    { text: `Показати втрати станом на ${todayDate()}` }
+  createIfNotExist(user);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    `Привіт ${ctx.message.from.username} 
+цей телеграм бот показує статистику втрат рашистів`,
+    {
+      reply_markup: {
+        keyboard: [
+          [{ text: `Показати втрати станом на ${todayDate()}` }],
+          [{ text: `Показати результати (будь яка дата)` }],
+          //   [{ text: `Збір коштів на допомогу армії` }],
+        ],
+        resize_keyboard: true,
+      },
+    }
+  );
+});
 
-                ],
-                [
-                    { text: `Показати результати (будь яка дата)` }
-
-                ],
-                [
-                    { text: `Збір коштів на допомогу армії` }
-
-                ]
-            ],
-            resize_keyboard: true,
-        },
-    })
-
-})
-
-bot.hears('Показати результати (будь яка дата)', ctx => {
-    ctx.reply(`Будь ласка, введіть дату 
+bot.hears("Показати результати (будь яка дата)", (ctx) => {
+  ctx.reply(`Будь ласка, введіть дату 
 у форматі : YYYY-MM-DD  
 Наприклад : 2022-07-15
 Зверніть увагу,запити здійснюються 
-з початку березня 2022 року`)
-})
-bot.hears('Збір коштів на допомогу армії', ctx => {
-    ctx.replyWithMarkdown(`
-Mono Bank : [4441 1144 5242 6750](https://send.monobank.ua/5xUEavwuX9)
+з початку березня 2022 року`);
+});
+// bot.hears('Збір коштів на допомогу армії', ctx => {
+//     ctx.replyWithMarkdown(`
+// Mono Bank : [4441 1144 5242 6750](https://send.monobank.ua/5xUEavwuX9)
 
--Навіть декілька гривень можуть врятувати життя-
-                            ❤️❤️❤️
-`)
-})
+// -Навіть декілька гривень можуть врятувати життя-
+//                             ❤️❤️❤️
+// `)
+// })
 bot.hears(`Показати втрати станом на ${todayDate()}`, async (ctx) => {
-    setTimeout(() => {
-        createImageWithToday(ctx)
-    }, 1000)
+  setTimeout(() => {
+    createImageWithToday(ctx);
+  }, 1000);
+});
 
-})
+bot.on("text", async (ctx) => {
+  const text = ctx.message.text;
+  validateDateFormat(text, ctx);
+});
 
-bot.on('text', async (ctx) => {
-    const text = ctx.message.text;
-    validateDateFormat(text, ctx)
-})
+bot.launch();
 
-bot.launch()
-
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
